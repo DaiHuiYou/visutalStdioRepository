877ac556d96fafe916f596f1b5246ffff29cf9ab
DaiHuiYou <daihuiyo@163.com>
2020-03-29 17:46:56 +0800

    添加项目文件。

---
+++ demo.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main() {
+    FILE* fp = fopen("write.txt", "w");
+    fputs("Real Python!", fp);
+    
+    
+    int res = fclose(fp);
+    printf("result : %d", res);
+
+    return 1;
+}
